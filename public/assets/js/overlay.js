b82cdf6d6da6bc9dad85f4bcd14f0f89d94d24f2
Charlie Astrada <charlieastrada@xagax.com>
2017-04-24 22:02:39 -0300

    Add overlay component for the mobile hamburger menu.
    * tweak the margin-top for hero h1.

---
+++ public/assets/js/overlay.js
@@ -0,0 +1,10 @@
+//JavaScript function for overlay component
+
+function openNav() {
+  document.getElementById("nav-primary").style.height = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("nav-primary").style.height = "0%";
+}
