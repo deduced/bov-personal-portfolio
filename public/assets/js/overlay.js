a5ec6aa82f06acf656beca209de9949a1431ae97
Charlie Astrada <charlieastrada@xagax.com>
2017-04-29 15:31:22 -0300

    Fixed issue with overlay trigger invoking on nav element.
    * Added a div to invoke that since we would otherwise be using the nav only for the mobile menu when it should be the container for all navs, mobile and otherwise.

--- public/assets/js/overlay.js
+++ public/assets/js/overlay.js
@@ -1,10 +1,10 @@
 //JavaScript function for overlay component
 
 function openNav() {
-  document.getElementById("nav-primary").style.height = "100%";
+  document.getElementById("mobile-nav").style.height = "100%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-  document.getElementById("nav-primary").style.height = "0%";
+  document.getElementById("mobile-nav").style.height = "0%";
 }
