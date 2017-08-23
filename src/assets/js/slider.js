1b20834b1141e2c33c3442dca13650a169cd9f2b
Charlie Astrada <charlieastrada@xagax.com>
2017-08-22 21:37:53 -0300

    Add comments for different sections in slider.js

--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -2,9 +2,12 @@
 
 $(document).ready(function() {
 
+  //set first slider button/item to active
+
   $('#slider-1 .c-slider__bullet').first().addClass('c-slider__bullet--active');
   $('#slider-1 .c-slider__item').first().addClass('c-slider__item--active');
 
+  //slider 1 click function
   $('#slider-1 .c-slider__bullet').click( function(){
     var $this = $(this);
     var $siblings = $this.parent().children();
@@ -14,9 +17,12 @@ $(document).ready(function() {
     $('#slider-1 .c-slider__bullet').removeClass('c-slider__bullet--active').eq(position).addClass('c-slider__bullet--active');
   });
 
+  //set second slider button/item to active
+
   $('#slider-2 .c-slider__bullet').first().addClass('c-slider__bullet--active');
   $('#slider-2 .c-slider__item').first().addClass('c-slider__item--active');
 
+  //slider 2 click function
   $('#slider-2 .c-slider__bullet').click( function(){
     var $this = $(this);
     var $siblings = $this.parent().children();
@@ -26,9 +32,12 @@ $(document).ready(function() {
     $('#slider-2 .c-slider__bullet').removeClass('c-slider__bullet--active').eq(position).addClass('c-slider__bullet--active');
   });
 
+  //set 3rd slider button/item to active
+
   $('#slider-3 .c-slider__bullet').first().addClass('c-slider__bullet--active');
   $('#slider-3 .c-slider__item').first().addClass('c-slider__item--active');
 
+  //slider 3 click function
   $('#slider-3 .c-slider__bullet').click( function(){
     var $this = $(this);
     var $siblings = $this.parent().children();
@@ -38,9 +47,12 @@ $(document).ready(function() {
     $('#slider-3 .c-slider__bullet').removeClass('c-slider__bullet--active').eq(position).addClass('c-slider__bullet--active');
   });
 
+  //set 4th slider button/item to active
+
   $('#slider-4 .c-slider__bullet').first().addClass('c-slider__bullet--active');
   $('#slider-2 .c-slider__item').first().addClass('c-slider__item--active');
 
+  //slider 4 click function
   $('#slider-4 .c-slider__bullet').click( function(){
     var $this = $(this);
     var $siblings = $this.parent().children();
@@ -50,9 +62,12 @@ $(document).ready(function() {
     $('#slider-4 .c-slider__bullet').removeClass('c-slider__bullet--active').eq(position).addClass('c-slider__bullet--active');
   });
 
+  //set 5th slider button/item to active
+
   $('#slider-6 .c-slider__bullet').first().addClass('c-slider__bullet--active');
   $('#slider-2 .c-slider__item').first().addClass('c-slider__item--active');
 
+  //slider 5 click function
   $('#slider-6 .c-slider__bullet').click( function(){
     var $this = $(this);
     var $siblings = $this.parent().children();
