8ad49caad1130a7069d362ac6b9ea991792e9e08
Charlie Astrada <charlieastrada@xagax.com>
2017-12-29 21:19:19 -0300

    Complete scrollto functionality using vanilla javascript.
    Used to be in jquery but was redone for project2 of Mastering the DOM.

--- src/assets/js/scrolling-nav.js
+++ src/assets/js/scrolling-nav.js
@@ -1,10 +1,38 @@
-//jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $(document).on('click', 'a.page-scroll', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+function scrollTo(e) {
+    var distanceToTop = function(el) {
+        try {
+            return Math.floor(el.getBoundingClientRect().top);
+        } catch(err) {
+            console.log(err);
+        }
+    };
+
+    var checkIfDone = setInterval(function() {
+        var atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+        if (distanceToTop(targetAnchor) === 0 || atBottom) {
+            targetAnchor.tabIndex = "-1";
+            targetAnchor.focus();
+            window.history.pushState("", "", targetID);
+            clearInterval(checkIfDone);
+        }
+    }, 100);
+
+    var targetID = this.getAttribute("href");
+    var targetAnchor = document.querySelector(targetID);
+    var originalTop = distanceToTop(targetAnchor);
+    var offset = document.querySelector('.l-nav-primary-desktop').offsetHeight;
+
+    e.preventDefault();
+
+    if (!targetAnchor) {
+        return;
+    }
+
+    window.scrollBy({ top: originalTop - offset, left: 0, behavior: "smooth" });
+
+}
+
+// grab links
+var linksToAnchors = document.querySelectorAll('a.page-scroll');
+// Event handler for clicking on links
+linksToAnchors.forEach(function(each) { each.onclick = scrollTo; });
